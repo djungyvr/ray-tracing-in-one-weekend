e29fe495759c9a1d145683ad21839639dc291ef2
David Jung <djungyvr@gmail.com>
2018-12-28 22:15:55 -0800

    Add basic sphere

--- main.cpp
+++ main.cpp
@@ -10,16 +10,36 @@ vec3 color(const ray& r) {
 	// this y direction has been shifted so we go
 	// from range [-1,1] to [0,2]
 	// which is then scaled down to [0,1]
-	float t = 0.5*(unitDir.y() + 1.0);
-	// Forms a linear interpolation (lerp)
+	float t = 0.5*(unitDir.y() + 1.0); // Forms a linear interpolation (lerp)
 	// Basically when t = 0 we want white
 	// And when t = 1 we want dominant blue,
 	// hence 0.5, 0.7 1.0
 	return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
-int main() {	
-	// Chapter 3
+bool hitSphere(vec3 center, ray p, float r) {
+	// recall eqn of sphere at origin 0 given by <v, v> = r^2
+	// where v is a vector [x,y,z]
+	// and with an origin that is offset by u
+	// <v-u, v-u> = r^2
+	// this results in
+	// to see if a ray p *hits* the sphere
+	// we simply insert v as p
+	// <p-u, p-u> = r^2
+	// <tB + A - C, tB + A - C> = r^2
+	// t^2*<B, B> + 2*t<B, A - C> + <A-C, A-C> - r^2 = 0
+	// The following has a soln if the descriminant b^2 - 4ac is greater than 0
+	vec3 A {p.origin()};
+	vec3 B {p.direction()};
+	vec3 C {center};
+	float a = inner(B, B);
+	float b = 2.0*inner(B, A - C);
+	float c = inner(A-C, A-C) - r*r;
+	return (b*b - 4*a*c > 0);
+}
+
+int main() {
+	// Chapter 4
 	int nx = 200;
 	int ny = 100;
 	std::cout << "P3\n" << nx << ' ' << ny << "\n255\n";
@@ -28,6 +48,7 @@ int main() {
 	vec3 htl(4.0, 0.0, 0.0);
 	vec3 vtl(0.0, 2.0, 0.0);
 	vec3 ogn(0.0, 0.0, 0.0);
+
 	for (int i = ny-1; i >= 0; --i) {
 		for (int j = 0; j < nx; j++) {
 			float u = float(j) / float(nx);
@@ -41,6 +62,9 @@ int main() {
 			// for each value of the y-component
 			ray r(ogn, llc + u*htl + v*vtl);
 			vec3 clr = color(r);
+			if (hitSphere(vec3(0,0,-5), r, 0.5)) {
+					clr = vec3(1, 0, 0);
+			}
 			int ir = int(255.99*clr.r());
 			int ig = int(255.99*clr.g());
 			int ib = int(255.99*clr.b());
@@ -51,3 +75,4 @@ int main() {
 	//draw(std::cout, 4, 4, "RRRRGGGGBBBBRRRR");
 	//drawGradient(std::cout, 152, 152);
 }
+
