f522553d82fad23d10feb9abb4e7e7ea6394eba8
David Jung <djungyvr@gmail.com>
2018-12-28 19:07:51 -0800

    Add vec3 class and some operators

---
+++ ppm.h
@@ -0,0 +1,71 @@
+/*
+ * Create an image by writing a PPM file
+ * The file looks like
+ * P3
+ * 3 2
+ * 255
+ * 255 0 0 0 255 0 0 0 255
+ * 255 0 0 0 255 0 0 0 255
+ * * The P3 means the colours are in ASCII
+ * With 3 columns and 2 rows
+ * Each RGB value maxed out at 255
+ */
+
+#include <cassert>
+#include <iostream>
+#include <string>
+
+void writePPMHeader(std::ostream& file, int width, int height) {
+	file << "P3" << '\n';
+	file << width << ' ' << height << '\n';
+	file << 255 << '\n';
+}
+
+void draw(std::ostream& file, int width, int height, const std::string& drawing) {
+	if (width * height != drawing.length()) {
+		throw std::invalid_argument("width * height != length of drawing");
+	}
+	writePPMHeader(file, width, height);
+	for (int i = 0; i < height; ++i) {
+		for (int j = 0; j < width; ++j) {
+			char color = drawing.at(width*i + j);
+			switch(color) {
+				case 'R':
+					file << "255 0 0";
+					break;
+				case 'G':
+					file << "0 255 0";
+					break;
+				case 'B':
+					file << "0 0 255";
+					break;
+				default:
+					file << "0 0 0";
+					break;
+			}
+			file << '\n';
+		}
+	}
+}
+
+void drawGradient(std::ostream& file, int width, int height) {
+	writePPMHeader(file, width, height);
+
+	// Gets less green as we go down
+	for (int i = height - 1; i >= 0; --i) {
+		// Gets more red as we go right
+		for (int j = 0; j < width; ++j) {
+			float r {float(j) / float(width)};
+			float g {float(i) / float(height)};
+			float b {0.2};
+
+			int ir = int(255.9*r);
+			int ig = int(255.9*g);
+			int ib = int(255.9*b);
+
+			file << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
+}
+
+
---
+++ vec3.h
@@ -0,0 +1,106 @@
+/*
+ * vec3 represents geometric vectors and colors
+ * Since functions defined in class I think it's implicitly inlined
+ */
+
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stdexcept>
+
+class vec3 {
+	public:
+		vec3() : e{0, 0, 0} {};
+		vec3(float e1, float e2, float e3) : e{e1, e2, e3} {};
+		float x() const { return e[0]; };
+		float y() const { return e[1]; };
+		float z() const { return e[2]; };
+
+		float r() const { return e[0]; };
+		float g() const { return e[1]; };
+		float b() const { return e[2]; };
+
+		float e1() const { return e[0]; };
+		float e2() const { return e[1]; };
+		float e3() const { return e[2]; };
+
+		void e1(float v) { e[0] = v; };
+		void e2(float v) { e[1] = v; };
+		void e3(float v) { e[2] = v; };
+
+		const vec3& operator+() const { return *this; };
+		vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); };
+		float operator[](int i) const {
+			if (i < 0 || i > 2)
+				throw std::out_of_range("i out of range");
+			return e[i];
+		};
+		float& operator[](int i) {
+			if (i < 0 || i > 2)
+				throw std::out_of_range("i out of range");
+			return e[i];
+		};
+
+		vec3& operator+=(const vec3& v) {
+			e[0] += v.e1();
+			e[1] += v.e2();
+			e[2] += v.e3();
+			return *this;
+		}
+		vec3& operator-=(const vec3& v) {
+			return operator+=(-v);
+		}
+
+		float norm() const {
+			return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+		}
+
+		float squaredNorm() const {
+			return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+		}
+
+	private:
+		float e[3];
+};
+
+// streams
+inline std::ostream& operator<<(std::ostream& os, const vec3& v) {
+	os << v.e1() << ' ' << v.e2() << ' ' << v.e3();
+	return os;
+}
+inline std::istream& operator>>(std::istream& is, vec3& v) {
+	float e1, e2, e3;
+	is >> e1 >> e2 >> e3;
+	v.e1(e1);
+	v.e2(e2);
+	v.e3(e3);
+	return is;
+}
+
+// scalar product
+inline vec3 operator*(float s, const vec3& v) {
+	return vec3(s*v.e1(), s*v.e2(), s*v.e3());
+}
+inline vec3 operator*(const vec3& v, float s) {
+	return vec3(s*v.e1(), s*v.e2(), s*v.e3());
+}
+
+// adding and subtracting vectors
+inline vec3 operator+(const vec3& v1, const vec3& v2) {
+	return vec3(v1.e1() + v2.e1(), v1.e2() + v2.e2(), v1.e3() + v2.e3());
+}
+inline vec3 operator-(const vec3& v1, const vec3& v2) {
+	return operator+(v1, -v2);
+}
+
+// inner and cross products
+inline float inner(const vec3& v1, const vec3& v2) {
+	return v1.e1()*v2.e1() + v1.e2()*v2.e2() + v1.e3()*v2.e3();
+}
+inline vec3 cross(const vec3& v1, const vec3& v2) {
+	return vec3(
+		v1.e2()*v2.e3() - v1.e3()*v2.e2(),
+		v1.e3()*v2.e1() - v1.e1()*v2.e3(),
+		v1.e1()*v2.e2() - v1.e2()*v2.e1()
+	);
+}
