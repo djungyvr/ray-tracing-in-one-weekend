f550edc0d4ea7ab0eb38c77a086c77f63ed931e7
David Jung <djungyvr@gmail.com>
2019-01-17 09:22:42 -0800

    Add chapter 5 code

--- main.cpp
+++ main.cpp
@@ -1,45 +1,8 @@
 #include "ray.h"
 #include <iostream>
+#include <cmath>
 
-// Given a ray returns a color
-// based on the magnitude of the y direction
-vec3 color(const ray& r) {
-	vec3 unitDir = r.direction().unit();
-	// In this case t is dependent on the
-	// y direction
-	// this y direction has been shifted so we go
-	// from range [-1,1] to [0,2]
-	// which is then scaled down to [0,1]
-	float t = 0.5*(unitDir.y() + 1.0); // Forms a linear interpolation (lerp)
-	// Basically when t = 0 we want white
-	// And when t = 1 we want dominant blue,
-	// hence 0.5, 0.7 1.0
-	return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
-}
-
-// Origin of l and p should be (0, 0, z)
-// for general 3d lines switch to finding determinant of 3d matrix
-// TODO: Write matrix class so that we can get the determinant of an
-// n x n matrix
-bool hitLine(ray l, ray p) {
-	vec3 A {l.origin()};
-	vec3 B {l.direction()};
-	vec3 C {p.origin()};
-	vec3 D {p.direction()};
-	float a1 = A.x();
-	float a2 = A.y();
-	float b1 = B.x();
-	float b2 = B.y();
-	float c1 = C.x();
-	float c2 = C.y();
-	float d1 = D.x();
-	float d2 = D.y();
-	float det = (b1*-d2) - (-d1*b2);
-	// we just wanna check if its close to zero
-	return det*det <= 0.000001;
-}
-
-bool hitSphere(vec3 center, ray p, float r) {
+float hitSphere(const vec3& center, const ray& r, float radius) {
 	// recall eqn of sphere at origin 0 given by <v, v> = r^2
 	// where v is a vector [x,y,z]
 	// and with an origin that is offset by u
@@ -51,13 +14,38 @@ bool hitSphere(vec3 center, ray p, float r) {
 	// <tB + A - C, tB + A - C> = r^2
 	// t^2*<B, B> + 2*t<B, A - C> + <A-C, A-C> - r^2 = 0
 	// The following has a soln if the descriminant b^2 - 4ac is greater than 0
-	vec3 A {p.origin()};
-	vec3 B {p.direction()};
-	vec3 C {center};
-	float a = inner(B, B);
-	float b = 2.0*inner(B, A - C);
-	float c = inner(A-C, A-C) - r*r;
-	return (b*b - 4*a*c > 0);
+	vec3 oc {r.origin() - center};
+
+	float a = inner(r.direction(), r.direction());
+	float b = 2.0*inner(oc, r.direction());
+	float c = inner(oc, oc) - radius*radius;
+	float discriminant {b*b - 4*a*c};
+	if (discriminant < 0) {
+		return -1.0;
+	}
+	return (-b - std::sqrt(discriminant) / (2.0*a));
+}
+
+
+// Given a ray returns a color
+// based on the magnitude of the y direction
+vec3 color(const ray& r) {
+	float t = hitSphere(vec3(0,0,-1), r, 0.5);
+	if (t > 0.0) {
+		vec3 N = (r.pointAt(t) - vec3(0,0,-1)).unit();
+		return 0.5*vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+	}
+	vec3 unitDir = r.direction().unit();
+	// In this case t is dependent on the
+	// y direction
+	// this y direction has been shifted so we go
+	// from range [-1,1] to [0,2]
+	// which is then scaled down to [0,1]
+	t = 0.5*(unitDir.y() + 1.0); // Forms a linear interpolation (lerp)
+	// Basically when t = 0 we want white
+	// And when t = 1 we want dominant blue,
+	// hence 0.5, 0.7 1.0
+	return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -84,11 +72,6 @@ int main() {
 			// for each value of the y-component
 			ray r(ogn, llc + u*htl + v*vtl);
 			vec3 clr = color(r);
-			if (hitSphere(vec3(0,0,-5), r, 0.5)) {
-					clr = vec3(1, 0, 0);
-			} else if (hitLine(ray(vec3(0.5, 1, -1), vec3(0.5, 0.5, 0.5)), r)) {
-					clr = vec3(0, 1, 0);
-			}
 			int ir = int(255.99*clr.r());
 			int ig = int(255.99*clr.g());
 			int ib = int(255.99*clr.b());
